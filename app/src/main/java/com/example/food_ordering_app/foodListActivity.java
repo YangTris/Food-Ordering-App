9c83bf3d5a51017dc243f5a6bc8983ebadbf5dcf
Khánh Phạm Phú <phamphukhanh.sgu.edu@gmail.com>
2024-03-18 00:05:09 +0700

    fix recyclerview display

--- app/src/main/java/com/example/food_ordering_app/foodListActivity.java
+++ app/src/main/java/com/example/food_ordering_app/foodListActivity.java
@@ -15,6 +15,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
 
 import com.bumptech.glide.Glide;
@@ -38,12 +39,11 @@ public class foodListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // setContentView(R.layout.activity_food_list);
-         setContentView(R.layout.food_list_recycleview);
+        // setContentView(R.layout.activity_food_list);
+        setContentView(R.layout.food_list_recycleview);
 
         final Context context = this;
-
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        toolbar.setTitle(getTitle());
 //
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -54,25 +54,72 @@ public class foodListActivity extends AppCompatActivity {
 //                context.startActivity(intent);
 //            }
 //        });
+        class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.ViewHolder> {
+            public FoodAdapter(Context mContext, ArrayList<Food> mFoods) {
+                this.mContext = mContext;
+                this.mFoods = mFoods;
+            }
 
-        final RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recyclerView_foodList);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            class ViewHolder extends RecyclerView.ViewHolder {
+                private ImageView mImageFood;
+                private TextView mTextName;
+                private TextView mTextDescription;
+
+                public ViewHolder(@NonNull View itemView) {
+                    super(itemView);
+                    mImageFood = itemView.findViewById(R.id.food_img);
+                    mTextName = itemView.findViewById(R.id.food_name);
+                    mTextDescription = itemView.findViewById(R.id.food_description);
+                }
+            }
+
+            private Context mContext;
+            private ArrayList<Food> mFoods;
+
+            @NonNull
+            @Override
+            public FoodAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                LayoutInflater inflater = LayoutInflater.from(mContext);
+                View foodView = inflater.inflate(R.layout.food_items, parent, false);
+                ViewHolder viewHolder = new ViewHolder(foodView);
+                return viewHolder;
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull FoodAdapter.ViewHolder holder, int position) {
+                Food food = mFoods.get(position);
+                Glide.with(mContext)
+                        .load(food.getImgURL())
+                        .placeholder(R.drawable.img_bg)
+                        .error(R.drawable.ic_launcher_background)
+                        .into(holder.mImageFood);
+                holder.mTextName.setText(food.getName());
+                holder.mTextDescription.setText(food.getDescription());
+            }
+
+            @Override
+            public int getItemCount() {
+                return mFoods.size();
+            }
+        }
+
+
 //        assert recyclerView != null;
-
 //        if (findViewById(R.id.idea_detail_container) != null) {
 //            mTwoPane = true;
 //        }
 
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView_foodList);
         foodService foodService = ServiceBuilder.buildService(foodService.class);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         Call<ArrayList<Food>> request = foodService.getIdeas();
 
         request.enqueue(new Callback<ArrayList<Food>>() {
             @Override
             public void onResponse(Call<ArrayList<Food>> request, Response<ArrayList<Food>> response) {
-                if(response.isSuccessful()){
-                    recyclerView.setAdapter(new FoodAdapter(foodListActivity.this,response.body()));
-
-                } else if(response.code() == 401) {
+                if (response.isSuccessful()) {
+                    recyclerView.setAdapter(new FoodAdapter(foodListActivity.this, response.body()));
+                } else if (response.code() == 401) {
                     Toast.makeText(context, "Your session has expired", Toast.LENGTH_LONG).show();
                 } else {
                     Toast.makeText(context, "Failed to retrieve items", Toast.LENGTH_LONG).show();
@@ -81,64 +128,15 @@ public class foodListActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<ArrayList<Food>> request, Throwable t) {
-                if (t instanceof IOException){
+                if (t instanceof IOException) {
                     Toast.makeText(context, "A connection error occured", Toast.LENGTH_LONG).show();
                 } else {
                     Toast.makeText(context, "Failed to retrieve items", Toast.LENGTH_LONG).show();
                 }
             }
         });
-    }
 
-//region Adapter Region
-    public class FoodAdapter
-            extends RecyclerView.Adapter<FoodAdapter.ViewHolder> {
-    public FoodAdapter(Context mContext, ArrayList<Food> mFoods) {
-        this.mContext = mContext;
-        this.mFoods = mFoods;
-    }
-    public class ViewHolder extends RecyclerView.ViewHolder {
-            private ImageView mImageFood;
-            private TextView mTextName;
-            private TextView mTextDescription;
 
-            public ViewHolder(@NonNull View itemView) {
-                super(itemView);
-                mImageFood = itemView.findViewById(R.id.food_img);
-                mTextName = itemView.findViewById(R.id.food_name);
-                mTextDescription = itemView.findViewById(R.id.food_description);
-            }
-        }
-
-        private Context mContext;
-        private ArrayList<Food> mFoods;
-
-        @NonNull
-        @Override
-        public FoodAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            LayoutInflater inflater = LayoutInflater.from(mContext);
-            View foodView = inflater.inflate(R.layout.food_items, parent, false);
-            ViewHolder viewHolder = new ViewHolder(foodView);
-            return viewHolder;
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull FoodAdapter.ViewHolder holder, int position) {
-            Food food = mFoods.get(position);
-            Glide.with(mContext)
-                    .load(food.getImgURL())
-                    .placeholder(R.drawable.img_bg)
-                    .error(R.drawable.ic_launcher_background)
-                    .into(holder.mImageFood);
-
-            holder.mTextName.setText(food.getName());
-            holder.mTextDescription.setText(food.getDescription());
-        }
-
-        @Override
-        public int getItemCount() {
-            return mFoods.size();
-        }
     }
 }
 //        @Override
