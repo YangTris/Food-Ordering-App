620047530f3f2a8e7cd634416c2212e5e828c42a
Khánh Phạm Phú <phamphukhanh.sgu.edu@gmail.com>
2024-03-19 12:08:21 +0700

    add mancj search bar dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
 
     // recyclerview
     implementation ("androidx.recyclerview:recyclerview:1.2.0-alpha01")
-
+    implementation ("com.github.mancj:MaterialSearchBar:0.8.5")
     // Logging
     implementation ("com.squareup.okhttp3:logging-interceptor:3.9.0")
 
@@ -54,4 +54,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
