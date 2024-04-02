eaa4eb68ef6ae421a09d6c35535d137b3e148aa2
Khánh Phạm Phú <phamphukhanh.sgu.edu@gmail.com>
2024-04-02 13:36:12 +0700

    add dummies data

--- app/src/main/java/com/example/food_ordering_app/LoginActivity.java
+++ app/src/main/java/com/example/food_ordering_app/LoginActivity.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.food_ordering_app.models.User;
 import com.google.android.material.textfield.TextInputLayout;
 
 
@@ -16,6 +17,14 @@ public class LoginActivity extends AppCompatActivity {
     private TextInputLayout usernameInput;
     private TextInputLayout passwordInput;
     private Button loginButton;
+
+
+    // Dummies:
+    User customer = new User("0", "John Doe", 0, "123 Main St", "1234567890", "john@example.com", "cus");
+
+    // Creating an admin
+    User admin = new User("1", "Admin User", 1, "456 Admin St", "0987654321", "admin@example.com", "admin");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
